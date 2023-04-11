bcc36cabc893843133ef0cfa41654a8ebd791817
Berenger Dalle-Cort <berenger@42borgata.com>
2023-04-10 23:40:56 -0400

    feat(merge-sort): handle n-sized arrays

--- src/shared/sorting/merge-sort/merge-sort.ts
+++ src/shared/sorting/merge-sort/merge-sort.ts
@@ -4,6 +4,8 @@
  * @param arr an array to sort
  * @returns a sorted array (may be the same reference if array is empty or small and sorted)
  */
+import {expect} from "@jest/globals";
+
 export function merge_sort<T>(arr: T[]): T[] {
     // Check the argument, this function is the public one and
     // could be called from JS
@@ -28,8 +30,47 @@ function merge_sort_implem<T>(arr: T[]): T[] {
             return arr[0] <= arr[1] ? arr : [ arr[1], arr[0] ];
         default:
             // split array in two
+            let [left, right] = split(arr);
             // sort each of them
+            left = merge_sort_implem(left);
+            right = merge_sort_implem(right);
             // merge sorted arrays
-            throw new Error("Not implemented yet")
+            return merge(left, right);
     }
+}
+
+/**
+ * Split a given array into two arrays
+ * when array size is odd, the first array is bigger, if not their size are identical.
+ * @param arr
+ */
+function split<T>(arr: T[]): [T[], T[]] {
+    const index_to_cut = Math.ceil( arr.length / 2 );
+    const left = arr.slice(0, index_to_cut);
+    const right = arr.slice(index_to_cut);
+    expect([...left, ...right]).toStrictEqual(arr);
+    return [left, right];
+}
+
+/**
+ * Merge two sorted arrays into a single sorted array
+ */
+function merge<T>(left: T[], right: T[]): T[] {
+    const result = new Array<T>();
+
+    // create two cursors to advance in both arrays
+    let left_cursor = 0, right_cursor = 0;
+    // while we didn't reach the end of each arrays...
+    while( left_cursor < left.length || right_cursor < right.length) {
+        // compare the numbers pointed on the left and on the right
+        // take the smallest and advance the cursor accordingly
+        if( right_cursor === right.length || left[left_cursor] <= right[right_cursor]) {
+            result.push(left[left_cursor]);
+            left_cursor++;
+        } else {
+            result.push(right[right_cursor]);
+            right_cursor++;
+        }
+    }
+    return result;
 }
