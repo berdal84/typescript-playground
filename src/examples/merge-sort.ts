a77fcb31110fb3ded06d9da137ac9cf280d2173e
berdal84 <berenger@42borgata.com>
2023-04-11 22:20:00 -0400

    feat(merge-sort): add example

---
+++ src/examples/merge-sort.ts
@@ -0,0 +1,32 @@
+import {merge_sort} from "@shared/sorting/merge-sort";
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Enter a list of numbers separated by a coma, we will sort it for you:\n>>> ', inputString => {
+    // to store not number input strings
+    const not_numbers: string[] = [];
+
+    // convert input string to number[]
+    const unsorted = inputString.trim().split(',').map(string => {
+        const n = Number(string);
+        if(isNaN(n)) not_numbers.push(string);
+        return n;
+    });
+
+    // abort if not numbers are found
+    if(not_numbers.length) {
+        throw new Error(`Those items are not numbers "${JSON.stringify(not_numbers)}"`)
+    }
+
+    process.stdout.write(`You entered: ${JSON.stringify(unsorted)}\n`);
+    console.log(`Sorting ...`);
+
+    const sorted = merge_sort(unsorted);
+
+    process.stdout.write(`Sorting DONE: `);
+    process.stdout.write(`${JSON.stringify(sorted)}\n`);
+    readline.close();
+});
