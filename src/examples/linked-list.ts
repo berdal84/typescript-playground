fbac86ce6f38ce510bbae23b85e6a9d77d0184d3
berdal84 <berenger@42borgata.com>
2023-04-11 22:19:45 -0400

    fix(linked-list): example

--- src/examples/linked-list.ts
+++ src/examples/linked-list.ts
@@ -1,12 +1,12 @@
-import {LinkedList} from '@shared';
+import {LinkedList} from '@shared/containers/linked-list';
 
 console.log('-- program begin --');
 
 const list = new LinkedList<string>('first', 'second', 'third');
 console.log(list.to_string());
-console.log(`The second element is: ${list.at(1)}`)
+console.log(`The second element is: ${list.at(1).data}`)
 console.log(`removing it...`)
 list.remove_at(1);
-console.log(`The second element is: ${list.at(1)}`)
+console.log(`The second element is: ${list.at(1).data}`)
 
 console.log('-- program end --');
