716a345053f75f4dcf87d50af1b558edce04bd61
Bérenger Dalle-Cort <berenger@42borgata.com>
2024-11-11 14:21:16 -0500

    add pipe/filter in functional example

--- src/examples/functional.ts
+++ src/examples/functional.ts
@@ -9,9 +9,25 @@
 // See https://gcanti.github.io/fp-ts/
 
 
-// Arrays
-import { makeBy } from 'fp-ts/Array'
+import { number } from 'fp-ts';
+import { array } from 'fp-ts';
+import { some } from 'fp-ts/Array';
+import { pipe } from 'fp-ts/lib/function';
+import { Predicate } from 'fp-ts/lib/Predicate';
 
+// Arrays
+/////////
+
+// makeBy
 const dozen = (n: number): number => n * 12
-const dozens = makeBy(10, dozen);
-console.log("makeBy(10, dozen): ", dozens);
+const ten_dozen = array.makeBy(10, dozen);
+console.log("makeBy(10, dozen): ", ten_dozen);
+
+// pipe and filter
+const data: number[] = [1,1,2,2,2,4,4,4,9];
+const even: Predicate<number> = (n) => n % 2 == 0;
+const even_numbers = pipe(
+    data,
+    array.filter( even )
+);
+console.log("even numbers: ", even_numbers);
